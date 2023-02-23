34f53820e9140706c6431acce0208839e6a4abbf
RubenMkda <83974135+RubenMkda@users.noreply.github.com>
2023-02-23 19:18:00 -0400

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'postgresql://fllvnawvuihaxp:79873c989178c12546c2d20b6d224ac5e4229aecb4415fd735e7203310aa61e5@ec2-52-204-157-26.compute-1.amazonaws.com:5432/de8jf9e9fknh92'
-
-#postgres://manolo_user:CEfdCGJDMO5UdmyhPlqkDHy208iqjirc@dpg-cfrrbkcgqg46pjod8oj0-a.oregon-postgres.render.com/manolo
+DATABASE_URI = 'postgresql://postgres:uszkXzOd4KoFqQ3tYBkg@containers-us-west-183.railway.app:6858/railway'
 
 def conectar():
     engine = create_engine(os.environ.get('DATABASE_URL'))
