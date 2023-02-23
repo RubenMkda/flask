41e8d6083e9a7255985fc23ffbfa9cc68fd60556
RubenMkda <83974135+RubenMkda@users.noreply.github.com>
2023-02-23 19:20:21 -0400

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URI = 'postgresql://postgres:uszkXzOd4KoFqQ3tYBkg@containers-us-west-183.railway.app:6858/railway'
 
 def conectar():
-    engine = create_engine(os.environ.get('DATABASE_URL'))
+    engine = create_engine(DATABASE_URI)
     Session = sessionmaker(bind=engine)
     s = Session()
 
